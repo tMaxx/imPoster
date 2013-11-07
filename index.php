a3b3b0507b900f67a82c2583268829dc0d42c4ee
tMaxx <mikwaw@gmail.com>
2013-11-07 14:51:06 +0100

    Some updates

--- index.php
+++ index.php
@@ -2,6 +2,9 @@
 define('ROOT', realpath(dirname(__FILE__)));
 define('CWD', getcwd());
 
+define('REQUEST', $_GET['_req']);
+unset($_GET['_req']);
+
 //redirect to init file, nothing else to do here
 include_once 'init/index.php';
 die;
---
+++ init/cache.php
@@ -0,0 +1,4 @@
+<?php ///revCMS /init/cache.php
+
+$TAB['INIT'] = array();
+$TAB['INCLUDE'] = array();
