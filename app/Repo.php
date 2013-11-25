ffe72da5302d6dd6bfd3291e7ef7cbec43bdacda
Nicolas <mikwaw@gmail.com>
2013-11-25 17:17:16 -0500

    Add find to Repo

--- app/Repo.php
+++ app/Repo.php
@@ -1,9 +1,14 @@
 <?php ///teo /app/Repo.php
-
+///Repository class
 class Repo {
     ///Related object name
     static $OBJ;
 
+    /**
+     * Create and set object
+     * @param $a array
+     * @return \Model
+     */
     public static function row(array $a)
     {
         $r = new static::$OBJ;
@@ -11,6 +16,18 @@ class Repo {
         return $r;
     }
 
+    ///Get $OBJ's table
+    public static function table()
+    {
+        $t = static::$OBJ;
+        return $t::table();
+    }
+
+    /**
+     * Find object by given id
+     * @param $id
+     * @retun NULL|\Model
+     */
     public static function findById($id)
     {
         $r = DB::row('SELECT * FROM '.self::table().' WHERE '.strtolower(self::$OBJ).'_id=?', 'i', array($id));
@@ -19,20 +36,51 @@ class Repo {
         else
             return null;
     }
-    
+
+    ///Find all objects
     public static function findAll()
     {
         $rows = DB::rows('SELECT * FROM '.self::table());
         $r = array();
         foreach($rows as $v)
             $r[] = self::row($v);
-        
+
         return $r;
     }
-    
-    public static function table()
+
+    /**
+     * Find all objects by given rules
+     * @param $where rules
+     * @param $and or or
+     * @return objects
+     */
+    public static function find(array $where, $and = TRUE)
     {
-        $t = static::$OBJ;
-        return $t::table();
+        //przemiel $where na postać 'klucz=wartość'
+        //escape wartości, param guess mysqli - yyy.. ze co tutaj? :D - właśnie szukam gdzie to było
+		if ($and) {
+	       $param = '(1=1)';
+	       $glue = ' and '
+		} else {
+		    $param = '(1=2)';
+		    $glue = ' or ';
+		}
+        foreach ($where as $k => $v) {
+        	//support multiple arguments
+        	if (is_array($v))
+        		$v = ' in ("'.implode('","', $v).'")';
+        	else
+        		$v = '='.$v;
+        	$param .= $glue.$k.$v;
+        }
+        	//WOW!
+        	//oj tam oj tam
+        $rows = DB::rows('SELECT * FROM '.self::table().' WHERE '.$param);
+
+        $ret = array();
+		foreach ($rows as $row)
+	        $ret[] = self::row($row);
+
+        return $ret;
     }
 }
