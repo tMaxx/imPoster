31b0e98fdadccd1df6a832a15aff7c500a4b3c06
dervill <idervill@gmail.com>
2013-12-09 20:18:23 +0100

    Login form

--- view/user/login.php
+++ view/user/login.php
@@ -1,1 +1,12 @@
-here be login form
+<?php
+?>
+<html>
+    <body>
+        
+        Login: <td align="center"><input type="text" name="login" size="3" maxlength="20" />
+		Pass: <td align="center"><input type="text" name="password" size="3" maxlength="20" /> 
+           <input type="submit" value="Zaloguj" />
+        
+    </body>
+</html>
+<?php
