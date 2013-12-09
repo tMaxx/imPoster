479add8eb30d05c2eae16ff83d895b01eb4f568a
dervill <idervill@gmail.com>
2013-12-09 20:26:18 +0100

    login v3

--- view/user/login.php
+++ view/user/login.php
@@ -1,9 +1,9 @@
 <?php
 ?>
-<form action="trocheinaczej.php" method="post">
-        
-        Login: <td align="center"><input type="text" name="login" size="3" maxlength="20" />
-		Pass: <td align="center"><input type="text" name="password" size="3" maxlength="20" /> 
-           <input type="submit" value="Zaloguj" />
-        
+    <form action="trocheinaczej.php" method="post">
+
+            Login: <input type="text" name="login" size="3" maxlength="20" />
+            Pass: <input type="text" name="password" size="3" maxlength="20" /> 
+               <input type="submit" value="Zaloguj" />
+    </form>
 <?php
