d4a6efd528076ce75b02f1cb180975cfe2477f6f
tMaxx <mikwaw@gmail.com>
2013-12-09 21:21:59 +0100

    Bump

--- sys/Form.php
+++ sys/Form.php
@@ -4,13 +4,42 @@
  * Simplyfy all stuff ;)
  */
 
-class Form
-{
+class Form {
+
+	protected $fields;
+	protected $values;
+	protected $name;
+
 	/**
 	 * @todo everything
+	 * @param $def
+	 * array(
+	 * 'name' => 'nazwwaaaa',
+	 * 'fields' => array(
+	 *     'pole' => array('typ', 'zmienna' => 'wartosc')
+	 *     'pole2' => array('typx', 'zmienna' => 'wartosc')
+	 *     )
+	 * )
 	 */
-	function __construct()
-	{
+	function __construct($def) {
+		if (!is_array($def))
+			throw new ErrorCMS("Type not yet supported");
+			
+		$this->name = $def['name'];
+		$this->fields = $def['fields'];
+		$this->values = array();
+	}
 
+	public function set(array $in) {
+		
+	}
+
+	public function get($key = NULL) {
+		
+	}
+
+	///Render form
+	public function r() {
+		
 	}
 }
