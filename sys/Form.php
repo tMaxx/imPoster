e9088262f33b5d4d2ffd9b109ddda29eef221863
tMaxx <mikwaw@gmail.com>
2013-12-10 21:54:46 +0100

    Fix indent

--- sys/Form.php
+++ sys/Form.php
@@ -31,51 +31,50 @@ class Form {
 	}
 
 	public function set(array $in) { 
-	   $r = array();
-       foreach ($in as $k => $v)
-         if(array_key_exists($k, $this->fields))
-            $this->values[$k] = $v;
-           
-        return $this;
+		$r = array();
+		foreach ($in as $k => $v)
+			if(array_key_exists($k, $this->fields))
+				$this->values[$k] = $v;
+
+		return $this;
 	}
 
 	public function get($key = NULL) {
 		if(isset($key))
-        {
-            if(is_array($key))
-             {
-                $r = array();
-                foreach($key as $v)
-                    $r[$v] = array_key_exists($v, $this->values) ? $this->values[$v] : NULL;
-                return $r;
-            }
-            elseif(is_string($key))
-            {
-                $r = array_key_exists($key, $this->values) ? $this->values[$key] : NULL;
-                return $r;
-            }
-        }
-        else
-            return $this->values;
-    }
+		{
+			if(is_array($key))
+			 {
+				$r = array();
+				foreach($key as $v)
+					$r[$v] = array_key_exists($v, $this->values) ? $this->values[$v] : NULL;
+				return $r;
+			}
+			elseif(is_string($key))
+			{
+				$r = array_key_exists($key, $this->values) ? $this->values[$key] : NULL;
+				return $r;
+			}
+		}
+		else
+			return $this->values;
+	}
 	///Render form
 	public function r() {
 		echo '<form method="post">';
-        foreach ($this->fields as $k => $v) {
+		foreach ($this->fields as $k => $v) {
 			$val = isset($this->values[$k]) ? $this->values[$k] : '';
 			$name = $this->name.'::'.$k;
-            switch($v[0]) {
-                case 'password':
-                    echo '<input type="password" name="', $name,'" value="', $val, '" />';
-                    break;
-                case 'text':
-                    echo '<input type="text" name="', $name, '" value="', $val, '" />';
-                    break;
-                case 'submit':
-                    echo '<input type="submit" name="', $name, '" value="Zaloguj" />';
-                    break;
-            }
-        }
-        echo '</form>';
+			$type = $v[0];
+			$attr = '';
+			switch ($type) {
+				case 'email':
+				case 'password':
+				case 'text':
+				case 'submit':
+					echo '<input type="', $type, '" name="', $name,'" ', $attr, ' value="', $val, '" />';
+					break;
+			}
+		}
+		echo '</form>';
 	}
 }
